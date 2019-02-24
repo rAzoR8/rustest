6ef42036ac3c912a896fab9d6b785d6596993291
Fabian Wahlster <wahlster@ux3d.io>
2019-02-24 09:26:52 +0100

    more quaternion impl

--- src/strahl/quaternion.rs
+++ src/strahl/quaternion.rs
@@ -1,4 +1,5 @@
 use super::vec::*;
+use super::mat::*;
 
 use std::ops::{Mul, MulAssign};
 
@@ -8,36 +9,45 @@ pub struct Quat
     pub q: Vec4
 }
 
-impl Quat
-{
-    pub fn new(axis: &Vec4, angle_deg: f32) -> Quat
-    {
-        let h = angle_deg * std::f32::consts::FRAC_PI_2 / 180.0;
-        let mut q = h.sin() * axis;
-        q.set(h.cos(), 3);
-        Quat{q: q}
+impl Quat {
+    // Identity
+    pub fn new() -> Quat {
+        Quat {
+            q: Vec4::new(0.0, 0.0, 0.0, 1.0),
+        }
     }
 
-    pub fn conjugate(&self) -> Quat
-    {
+    pub fn from_axis_angle_rad(axis: &Vec4, angle_rad: f32) -> Quat {
+        let h = angle_rad * 0.5;
+        let mut q = h.sin() * axis;
+        q.set(h.cos(), 3);
+        Quat { q: q }
+    }
+
+    pub fn from_axis_angle_deg(axis: &Vec4, angle_deg: f32) -> Quat {
+        Quat::from_axis_angle_rad(axis, angle_deg * std::f32::consts::PI / 180.0)
+    }
+
+    pub fn conjugate(&self) -> Quat {
         Quat::from(self.q * Vec4::new(-1.0, -1.0, -1.0, 1.0))
     }
 
-    pub fn inverse(&self) -> Quat
-    {
+    pub fn inverse(&self) -> Quat {
         Quat::from(self.conjugate().q / self.q.square_length())
     }
 
     // w component of point v should be 0.0
-    pub fn rotate(&self, v: &Vec4) -> Vec4
-    {
+    pub fn rotate(&self, v: &Vec4) -> Vec4 {
         (self * Quat::from(v) * self.inverse()).q
     }
 
-    fn multiply(&self, o: &Quat) -> Quat {
+    pub fn rotate_unit(&self, v: &Vec4) -> Vec4 {
+        (self * Quat::from(v) * self.conjugate()).q
+    }
 
+    fn multiply(&self, o: &Quat) -> Quat {
         //https://gist.github.com/mattatz/40a91588d5fb38240403f198a938a593
-		//var.xyz = q2.xyz * q1.w + q1.xyz * q2.w + cross(q1.xyz, q2.xyz);
+        //var.xyz = q2.xyz * q1.w + q1.xyz * q2.w + cross(q1.xyz, q2.xyz);
         //var.w = q1.w * q2.w - dot(q1.xyz, q2.xyz);
 
         let w1 = self.q.w();
@@ -47,6 +57,17 @@ impl Quat
         r.set(w1 * w2 - self.q.dot3(&o.q), 3);
 
         Quat::from(r)
+    }
+
+    pub fn to_mat3(&self) -> Mat4 {
+        Mat4 {
+            c: [
+                self.rotate_unit(&Vec4::from3(1.0, 0.0, 0.0)),
+                self.rotate_unit(&Vec4::from3(0.0, 1.0, 0.0)),
+                self.rotate_unit(&Vec4::from3(0.0, 0.0, 1.0)),
+                Vec4::new(0.0, 0.0, 0.0, 1.0),
+            ],
+        }
     }
 }
 
@@ -63,6 +84,16 @@ impl From<Vec4> for Quat {
 impl From<&Vec4> for Quat {
     fn from(o: &Vec4) -> Self {
         Quat { q: *o }
+    }
+}
+
+//######################################################################
+// Into
+//######################################################################
+
+impl Into<Mat4> for Quat {
+    fn into(self) -> Mat4 {
+        self.to_mat3()
     }
 }
 
@@ -99,5 +130,17 @@ impl Mul<&Quat> for &Quat {
 
     fn mul(self, o: &Quat) -> Quat {
         self.multiply(&o)
+    }
+}
+
+impl MulAssign<Quat> for Quat {
+    fn mul_assign(&mut self, o: Quat) {
+        *self = self.multiply(&o)
+    }
+}
+
+impl MulAssign<&Quat> for Quat {
+    fn mul_assign(&mut self, o: &Quat) {
+        *self = self.multiply(&o)
     }
 }
